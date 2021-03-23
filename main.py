2268956206e86fe1df895d41fef40a228711fad6
lit9999999999 <sola.lai.0000@gmail.com>
2021-03-23 21:22:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+sentence = ("TODAY IS CLOUDY")
+print(sentence.lower())
+
+sen2 = ("Have a great spring break")
+print(sen2.upper())
+
+sen3 = ("I like python")
+print(len(sen3))
+
+str1 = ("I am learning how to code in python")
+check ="learning" in str1
+print(check)
+
+
+#how to format the given string
+
+
