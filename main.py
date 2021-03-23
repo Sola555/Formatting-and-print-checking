a7d1828c22c2c8c01e59cd586e3e2d65a056940b
lit9999999999 <sola.lai.0000@gmail.com>
2021-03-23 21:34:13 +0000

    Added formatting functions

--- main.py
+++ main.py
@@ -13,5 +13,13 @@ print(check)
 
 
 #how to format the given string
-
-
+#putting variable inside {}
+place = input("What is your favourite place to visit? ")
+print(f"Hello, your favourite place is {place}")
+#using .format and give variable (multiple variables used)
+name = input("What is your name?")
+age = input('How old are you? ')
+print("Hello, {0}you are {1} Yeas old !".format(name,age))
+#Old version. "Words" + variable + "Words"
+animal = input ("What is your favourite animal?")
+print("Hello, your favourite animal is a " + animal + "!")
